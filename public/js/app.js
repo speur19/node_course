414b6eafb173589c56a5698dc0be0e7f6197db32
Bolsa <s.freelance01@gmail.com>
2020-07-25 10:39:30 -0500

    adding new files

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log('client side javascript file is loadades')
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector("input")
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    console.log(location)
+
+fetch('http://api.weatherstack.com/current?access_key=52d0e4628db67c7290c87e1e4f309e84&query='+ location). then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error.info)
+        }else{
+            console.log(data.request.query)
+        }
+    })
+})
+
+})
---
+++ src/app.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+
+const app = express()
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+app.use(express.static(publicDirectory))
+hbs.registerPartials(partialsPath)
+
+
+app.get('', (req, res) => {
+    //res.send() - For static route
+    res.render('index', {
+        title: 'index page',
+        name: 'Name of index',
+        footer: 'created by BagMan'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        name: 'About page',
+        footer: 'created by BagMan'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        name: 'HELP page',
+        footer: 'created by BagMan'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send(
+            'Still need the address'
+        )}
+    res.send({
+        forecast: 'it is snowing',
+        location: 'Mexico',
+        address: req.query.address
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        name: '404',
+        footer: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        name: '404',
+        message: 'page not found'  
+    })
+})
+
+
+
+app.listen(3000, () =>{
+    console.log('Server running at 3001 port')
+})
